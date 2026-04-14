320024160ca0efdba3d302c128e69048572c6ea0
ArturoCarrada <>
2026-04-14 15:38:31 -0600

    Se añade parte de email

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -3,26 +3,41 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email-service";
 
 
 const fileSystemLogRepository = new LogRepositoryImpl(
     new FileSystemDataSource()
 );
 
+const emailService = new EmailService();
 
 export class Server {
 
     public static async start() {
         console.log('Server started...');
-        
-        console.log(envs.MAILER_SECRET_KEY, envs.MAILER_EMAIL);
+
+        // console.log(envs.MAILER_SECRET_KEY, envs.MAILER_EMAIL);
+
+
+        //todo: Mandar email
+        // new SendEmailLogs(
+        //   emailService, 
+        //   fileSystemLogRepository,
+        // ).execute(
+        //   ['fernando.herrera85@gmail.com','fernando.herrera.cr@gmail.com']
+        // )
+        // emailService.sendEmailWithFileSystemLogs(
+        //   ['fernando.herrera85@gmail.com','fernando.herrera.cr@gmail.com']
+        // );
+
 
         CronService.createJob(
             '*/3 * * * * *',
             () => {
                 console.log(new Date);
                 // const checkService = new CheckService().execute('http://localhost:3000/posts/');    //Prueba con JSON Server (otro proyecto)
-                
+
                 const url = 'http://google.com';
                 // const url = 'http://localhost:3000/posts/';
                 const checkService = new CheckService(
@@ -32,11 +47,11 @@ export class Server {
                     undefined,
                     undefined,
                 )
-                .execute(url);
-                
+                    .execute(url);
+
             }
         );
-        
+
     }
 
 
